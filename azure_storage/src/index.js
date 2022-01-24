ce48b6d76b13952ef3d1e59b0b1d2d26360c55f0
Adrian <colid.bondzio@gmail.com>
2022-01-24 18:09:13 +0100

    added mongodb

--- azure_storage/src/index.js
+++ azure_storage/src/index.js
@@ -43,4 +43,76 @@ app.get("/video", (req, res) => {
 app.listen(PORT, () => {
   console.log('Microservice online');
 });
+// Loads the azure_storage package to interact with API
+const express = require("express")
+const azure = require('aws-s3');
+
+const app = express();
+
+//Throws an error if any of the required environment variables is missing.
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
+
+if (!process.env.STORAGE_ACCOUNT_NAME) {
+    throw new Error("Please specify the name of an Azure storage account in environment variable STORAGE_ACCOUNT_NAME.");
+}
+
+if (!process.env.STORAGE_ACCESS_KEY) {
+    throw new Error("Please specify the access key to an Azure storage account in environment variable STORAGE_ACCESS_KEY.");
+}
+
+// Extracts environment variables to globals
+const PORT = process.env.PORT;
+const STORAGE_ACCOUNT_NAME = process.env.STORAGE_ACCOUNT_NAME;
+const STORAGE_ACCESS_KEY = process.env.STORAGE_ACCESS_KEY;
+
+console.log(`Serving videos from Azure storage account ${STORAGE_ACCOUNT_NAME}.`);
+
+
+// Creates the Blob service API to communicate with Azure Storage.
+function createBlobService() {
+    const blobService = azure.createBlobService(STORAGE_ACCOUNT_NAME,
+        STORAGE_ACCESS_KEY);
+        return blobService;
+}
+
+// HTTP Get route for retrieving a video from azure_storage
+app.get("/video", (req, res) => {
+    // Specify the path to the video in storage as HTTP query parameter
+    const videoPath = req.query.path;
+    console.log(`Streaming video from path ${videoPath}.`);
+    // Connects to the azure_storage API
+    const blobService = createBlobService();
+    // Hard-coded container name
+    const containerName = "videos";
+    // Retrieve the video's properties from azure_storage
+    blobService.getBlobProperties(containerName, videoPath, (err, properties) => {
+             if (err) {
+                 // ... error handling ...
+                 console.error(`Error occurred getting properties for video ${containerName}/${videoPath}.`);
+                 console.error(err && err.stack || err);
+                 res.sendStatus(500);
+                 return;
+             }
+             // Writes HTTP headers to the response.
+             res.writeHead(200, {
+                 "Content-Length": properties.contentLength,
+                 "Content-Type": "video/mp4",
+             });
+             // Streams the video from azure_storage
+             blobService.getBlobToStream(containerName, videoPath, res, err => {
+                    if (err) {
+                        console.error(`Error occurred getting video ${containerName}/${videoPath} to stream.`);
+                        console.error(err && err.stack || err);
+                        res.sendStatus(500);
+                        return;
+                    }
+                });
+         });
+});
+
+app.listen(PORT, () => {
+    console.log('Microservice online');
+});
 
--- video_streaming/src/index.js
+++ video_streaming/src/index.js
@@ -17,47 +17,51 @@ const DBNAME = process.env.DBNAME;
 
 function main() {
 
-        return mongodb.MongoClient.connect(DBHOST)
-          .then(client => {
-                  const db = client.db(DBNAME);
-                  const videoCollection = db.collection("videos");
-                  app.get("/video", (req,res) => {
-                    const videoId = new mongodb.ObjectID(req.query.id);
-                    videosCollection.findOne({ _id: videoId}).then(videoRecord => {
-                      if (!videoRecord) {
-                        res.sendStatus(404);
-                        return;
-                      }
+  return mongodb.MongoClient.connect(DBHOST).then(client => {
+    const db = client.db(DBNAME);
+    const videosCollection = db.collection("videos");
 
-                      const forwardRequest = http.request(
-                        {
-                          host: VIDEO_STORAGE_HOST,
-                          port: VIDEO_STORAGE_PORT,
-                          path: `/video?path=$(videoRecord.videoPath)`,
-                          method: 'GET',
-                          headers: req.headers
-                        },
+    app.get("/video", (req,res) => {
+      const videoId = new mongodb.ObjectID(req.query.id);
+      videosCollection.findOne({_id: videoId}).then(videoRecord => {
+        if (!videoRecord) {
+          res.sendStatus(404);
+          return;
+        }
 
-                        forwardResponse => {
-                          res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
-                          forwardResponse.pipe(res);
-                        } 
-                  );
-                  
-                  req.pipe(forwardRequest);
-              })
-              .catch(err => {
-                console.error("Database query failed.");
-                console.error(err && err.stack || err);
-                res.sendStatus(500);
-              });
-        });
-          })
+        const forwardRequest = http.request(
+          {
+            host: VIDEO_STORAGE_HOST,
+            port: VIDEO_STORAGE_PORT,
+            path: `/video?path=${videoRecord.videoPath}`,
+            method: 'GET',
+            headers: req.headers
+          },
+
+          forwardResponse => {
+            res.writeHeader(fowardResponse.statusCode, forwardResponse.headers);
+            forwardResponse.pipe(rest);
+          }
+        );
+
+      req.pipe(forwardRequest);
+    })
+    .catch(err => {
+      console.error("Database query failed.");
+      console.error(err && err.stack || err);
+      res.sendStatus(500);
+    });
+  });
+
+  app.listen(port, () => {
+    console.log(`Microservice online.`);
+  });
+});
 
 main()
-  .then(() => console.log("Microservice online."))
-  .catch(err => {
-    console.error("Microservice failed to start.");
-    console.error(err && err.stack || err);
-  });
+    .then( () => console.log("Microservice online."))
+    .catch(err => {
+      console.error("Microservice failed to start.");
+      console.error(err && err.stack || err);
+     });
 }
